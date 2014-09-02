85f99680447ab2c995e0acf55067bcb48e337679
Yura <otan256@gmail.com>
2014-09-02 23:50:12 +0300

    small model refactoring

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,29 +1,30 @@
 class Reservation < ActiveRecord::Base
   validates_presence_of :table_id, :start_time, :end_time
-  validate :valid_datetime_fields
+  validate :valid_time_fields
 
-  before_create :validate_time
-  before_update :validate_time_with_id
+  before_create do
+    validate_time
+  end
+
+  before_update do
+    validate_time(with_id = true)
+  end
 
   private
 
-  def validate_time
-    unless self.class.where(["table_id = ? AND (start_time < ? AND end_time > ?)", table_id, end_time, start_time]).empty?
-      errors[:base] << "Table is already booked at this time"
-    end
+  def validate_time(with_id = false)
+    condition = if with_id
+                  self.class.where(["table_id = ? AND id <> ? AND (start_time < ? AND end_time > ?)", table_id, id, end_time, start_time]).empty?
+                else
+                  self.class.where(["table_id = ? AND (start_time < ? AND end_time > ?)", table_id, end_time, start_time]).empty?
+                end
+    errors[:base] << "Table is already booked at this time" unless condition
     errors.empty?
   end
 
-  def validate_time_with_id
-    unless self.class.where(["table_id = ? AND id <> ? AND (start_time < ? AND end_time > ?)", table_id, id, end_time, start_time]).empty?
-      errors[:base] << "Table is already booked at this time"
-    end
-    errors.empty?
-  end
-
-  def valid_datetime_fields
-    errors.add(:start_time, 'must be a valid datetime') if ((DateTime.parse(start_time.to_s) rescue ArgumentError) == ArgumentError)
-    errors.add(:end_time, 'must be a valid datetime') if ((DateTime.parse(end_time.to_s) rescue ArgumentError) == ArgumentError)
+  def valid_time_fields
+    errors.add(:start_time, 'must be a valid datetime') unless start_time.is_a? Time
+    errors.add(:end_time, 'must be a valid datetime') unless end_time.is_a? Time
   end
 
 end
